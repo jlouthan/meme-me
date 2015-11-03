6eda6452eba5a20e91d7c48bd7150609156b6ff7
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-11-03 15:56:26 -0800

    Select images from camera or album

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var memeImage: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func presentImagePickerWithSourceType(type: UIImagePickerControllerSourceType) -> Void {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = type
+        self.presentViewController(controller, animated: true, completion: nil)
     }
-
+    
+    @IBAction func pickImageFromAlbum(sender: AnyObject) {
+        presentImagePickerWithSourceType(UIImagePickerControllerSourceType.PhotoLibrary)
+    }
+    
+    
+    @IBAction func pickImageFromCamera(sender: AnyObject) {
+        presentImagePickerWithSourceType(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        if let newImage = info["UIImagePickerControllerOriginalImage"] {
+            self.memeImage.image = newImage as? UIImage
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
