8813022c6b17d901f518d392517e96ae433cb9bd
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-11-03 16:44:08 -0800

    Top and bottom text fields with some styling in viewDidLoad

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var memeImage: UIImageView!
     
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topMemeText: UITextField!
+    
+    @IBOutlet weak var bottomMemeText: UITextField!
+    
     
     override func viewWillAppear(animated: Bool) {
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
@@ -20,6 +25,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.topMemeText.backgroundColor = UIColor.clearColor()
+        self.bottomMemeText.backgroundColor = UIColor.clearColor()
+        self.topMemeText.borderStyle = UITextBorderStyle.None
+        self.bottomMemeText.borderStyle = UITextBorderStyle.None
+        self.topMemeText.text = "TOP"
+        self.bottomMemeText.text = "BOTTOM"
+        
+//        TODO maybe move this into delegate class?
+        self.topMemeText.delegate = self
+        self.bottomMemeText.delegate = self
     }
     
     func presentImagePickerWithSourceType(type: UIImagePickerControllerSourceType) -> Void {
@@ -38,6 +53,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         presentImagePickerWithSourceType(UIImagePickerControllerSourceType.Camera)
     }
     
+    //    image picker delegate
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         self.dismissViewControllerAnimated(true, completion: nil)
         if let newImage = info["UIImagePickerControllerOriginalImage"] {
@@ -47,6 +64,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+//    text field delegate
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (textField == self.topMemeText && textField.text == "TOP") || (textField == self.bottomMemeText && textField.text == "BOTTOM") {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 }
