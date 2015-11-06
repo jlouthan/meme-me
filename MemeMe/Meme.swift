08ab3f03500648ea5c2ad962a949e782c931825e
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-11-05 16:02:36 -0800

    Removing unnecessary initializer in struct

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -10,16 +10,8 @@ import Foundation
 import UIKit
 
 struct Meme {
-    
     let topText: String
     let bottomText: String
     let image: UIImage
     let memedImage: UIImage
-    
-    init(topText: String, bottomText: String, image: UIImage, memedImage: UIImage) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.image = image
-        self.memedImage = memedImage
-    }
 }
