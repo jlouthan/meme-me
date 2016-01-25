5f5834d97cf3a1b6b9336a231b8b42e6df7296b6
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-01-24 16:30:31 -0800

    Nav Bar with plus button presents New Meme view

---
+++ MemeMe/SentMemesTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Jennifer Louthan on 1/24/16.
+//  Copyright © 2016 JennyLouthan. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableViewController: UITableViewController {
+    
+    var memes: [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return memes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("MemeCell")!
+        let meme = memes[indexPath.row]
+        
+        cell.textLabel?.text = meme.topText + meme.bottomText
+        cell.imageView?.image = meme.memedImage
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+    }
+    
+}
+
